7c5b261bb03655e791d3fc6fd11e2be1984c97b7
Raymond <raymondlugo64@gmail.com>
2025-10-17 13:28:21 -0400

    Inicializaçao do projeto com variaveis protegidas.

---
+++ Vcard.py
@@ -0,0 +1,11 @@
+import os
+import re
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+
